33f7b51ef6f9e2f50522b86fe54a65bff1204318
ChristophPracht <christoph.pracht93@gmail.com>
2020-05-06 03:23:21 +0200

    First Level Part 1

---
+++ SP6 FPS Game/Assets/Door_Clear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Clear : MonoBehaviour
+{
+
+    GameObject[] enemiesInTheScene;
+
+    private void Start()
+    {
+        GameObject[] enemiesInTheScene = GameObject.FindGameObjectsWithTag("elvl1");
+        Debug.Log(enemiesInTheScene[0]);
+    }
+
+    private void Update()
+    {
+        GameObject[] enemiesInTheScene = GameObject.FindGameObjectsWithTag("elvl1");
+
+        if (enemiesInTheScene.Length == 0)
+        {
+            Debug.Log("elvl1 cleared");
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ SP6 FPS Game/Assets/EnemySpwan.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpwan : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public Transform enemy1Pos;
+    public Transform enemy2Pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            Invoke("EnemySpawner", 0.5f);
+            Destroy(gameObject, 10);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+
+    }
+
+    void EnemySpawner()
+    {
+        Debug.Log("EnemySpwans");
+        Instantiate(enemy, enemy1Pos.position, enemy1Pos.rotation);
+        Instantiate(enemy, enemy2Pos.position, enemy2Pos.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
